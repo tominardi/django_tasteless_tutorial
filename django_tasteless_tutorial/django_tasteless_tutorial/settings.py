0e569102cd13b912783d72f99ad4677c392d1f8e
tominardi <tominardi@gmail.com>
2015-02-03 22:52:06 +0100

    Add a CSS file

--- django_tasteless_tutorial/django_tasteless_tutorial/settings.py
+++ django_tasteless_tutorial/django_tasteless_tutorial/settings.py
@@ -84,3 +84,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'django_tasteless_tutorial', 'templates'), )
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'django_tasteless_tutorial', 'static'), )
